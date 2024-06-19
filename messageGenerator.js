1a7193cc86a81f247249b3307c7ea2a33fc80d24
Edward <pskwong@outlook.com>
2024-06-19 15:42:57 +0100

    Edwad_function_creature generator_finsihed

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,16 +1,34 @@
 // Code for Creature Generator --- Template for the Creature 
+function CreatureListGenerator(name = 'Human', type = 'Animal', habitat = 'City') {
+    let creature = {
+        name,
+        type,
+        habitat
+    };
+    creatureList.push(creature)
+}
 
 
 
 // Creature Information input -  (For Administrator ) 
-
+CreatureListGenerator('butterfly', 'animal', 'forest')
+CreatureListGenerator()
 
 
 
 // Creature Information Storage 
 
+var creatureList = [];
 
 
 
 // Function  - Message Generator 
+
+
+
+//Testing Area 
+
+
+
+console.log(creatureList)
 
