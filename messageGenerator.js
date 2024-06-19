bed787cdad074361290e320df7c148a3c741f083
Edward <pskwong@outlook.com>
2024-06-19 16:29:42 +0100

    Edward_function_message generator_finished

--- messageGenerator.js
+++ messageGenerator.js
@@ -5,30 +5,44 @@ function CreatureListGenerator(name = 'Human', type = 'Animal', habitat = 'City'
         type,
         habitat
     };
+    
     creatureList.push(creature)
 }
+
+// Creature Information Storage 
+
+var creatureList = new Array();
 
 
 
 // Creature Information input -  (For Administrator ) 
-CreatureListGenerator('butterfly', 'animal', 'forest')
-CreatureListGenerator()
+CreatureListGenerator('Clownfish', 'Fish', 'shallow, warm ocean waters with coral reefs')
+CreatureListGenerator('Red Fox', 'Mammal', 'various types of forests, including temperate and boreal forests')
+CreatureListGenerator('Bison', 'Mammal', 'open grasslands with abundant vegetation, such as prairies')
+CreatureListGenerator('Common Frog', 'Amphibian', 'freshwater ponds and lakes')
+CreatureListGenerator('Jaguar', 'Mammal', 'Warm, wet rainforests with dense vegetation and tall trees')
+CreatureListGenerator('Arctic Hare', 'Mammal', 'Cold, treeless tundra regions with frozen surfaces')
+CreatureListGenerator('Bees', 'Insect', 'diverse environments, from meadows to gardens')
 
 
 
-// Creature Information Storage 
 
-var creatureList = [];
 
 
 
 // Function  - Message Generator 
 
+function messageGenerator(input) {
+    const numOfCreature = input.length
+    const randomNum = Math.floor(Math.random() * (numOfCreature + 1))
+    const nameOfCreature = input[randomNum].name
+    const typeOfCreature = input[randomNum].type
+    const habitatOfCreature = input[randomNum].habitat
+
+    return `DO YOU KNOW that ${nameOfCreature} is ${typeOfCreature} who lives in ${habitatOfCreature}.`
+}
 
 
 //Testing Area 
 
-
-
-console.log(creatureList)
-
+console.log(messageGenerator(creatureList))
