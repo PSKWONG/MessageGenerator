0f4c62f12b2490e1178421c6cf1468934c9ea322
Edward <pskwong@outlook.com>
2024-06-19 14:26:14 +0100

    Program Structure Initiation

---
+++ messageGenerator.js
@@ -0,0 +1,16 @@
+// Code for Creature Generator --- Template for the Creature 
+
+
+
+// Creature Information input -  (For Administrator ) 
+
+
+
+
+// Creature Information Storage 
+
+
+
+
+// Function  - Message Generator 
+
